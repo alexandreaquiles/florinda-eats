187d15583abdb2e607c4301993bf7525f6d26d15
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2024-10-26 17:09:42 -0300

    endpoint cria novo item do cardápio na API

--- src/main/java/mx/florinda/itemCardapio/ItemCardapioResource.java
+++ src/main/java/mx/florinda/itemCardapio/ItemCardapioResource.java
@@ -1,8 +1,11 @@
 package mx.florinda.itemCardapio;
 
+import io.quarkus.hibernate.reactive.panache.Panache;
 import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import org.jboss.resteasy.reactive.RestResponse;
 
 import java.util.List;
 
@@ -12,5 +15,12 @@ public class ItemCardapioResource {
   @GET
   public Uni<List<ItemCardapio>> lista() {
     return ItemCardapio.listAll();
+  }
+
+  @POST
+  public Uni<RestResponse<ItemCardapio>> cria(ItemCardapioRequest itemCardapioRequest) {
+    ItemCardapio itemCardapio = itemCardapioRequest.toEntity();
+    return Panache.<ItemCardapio>withTransaction(itemCardapio::persist)
+            .map(item -> RestResponse.status(RestResponse.Status.CREATED, item));
   }
 }
